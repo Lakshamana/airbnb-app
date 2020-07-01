7f528a5bbeee939478acb85df0e4d4b96355a91a
Lakshamana <guitrompa1@gmail.com>
2020-07-01 14:39:31 -0300

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: false,
+  semi: false,
+  printWidth: 80,
+  allowParens: 'avoid'
+};
---
+++ src/service/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+  baseUrl: 'http://10.0.2.2:3333'
+})
